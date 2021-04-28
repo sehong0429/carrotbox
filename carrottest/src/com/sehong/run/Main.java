47076f23af9732e1cf2f6fa3b33564f7412c643d
sehong0429 <qkrdlgo1214@naver.com>
2021-04-28 23:32:54 +0900

    #03 HAPPYBIRTHDAY SEHONG -sehong-

--- carrottest/src/com/sehong/run/Main.java
+++ carrottest/src/com/sehong/run/Main.java
@@ -2,7 +2,10 @@ package com.sehong.run;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		System.out.println("HAPPYBIRTHDAY SEHONG");
+	}	
+	public String calcPlus(String num, String num2) {
+		return num+num2;
 	}
-
+	
 }
